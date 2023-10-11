e1fa7025c599dcc609d917d3e09e16219eb6c0d8
서창열 <kongbaguni@gmail.com>
2023-10-11 14:36:49 +0900

    Initial Commit

---
+++ myinbody/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myinbody
+//
+//  Created by Changyeol Seo on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myinbody/myinbodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myinbodyApp.swift
+//  myinbody
+//
+//  Created by Changyeol Seo on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct myinbodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
