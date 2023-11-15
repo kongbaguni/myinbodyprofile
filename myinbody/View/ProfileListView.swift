a42b234bc32b9d91ee8e5147bdd315b864e55b2e
chang-yul, Seo <kongbaguni@gmail.com>
2023-11-15 15:08:19 +0900

    fix bug

--- myinbody/View/ProfileListView.swift
+++ myinbody/View/ProfileListView.swift
@@ -27,62 +27,62 @@ struct ProfileListView: View {
     
     @State var isAlert:Bool = false
     
-    var body: some View {
-        List {
-            if profiles.count == 0 {
-                Section {
-                    Image("launchIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .padding(20)
-                }
-            } else {
-                Section {
-                    ForEach(profiles, id:\.self) { profile in
-                        NavigationLink {
-                            ProfileDetailView(profile: profile)
-                        } label: {
+    var profileList : some View {
+        ForEach(profiles, id:\.self) { profile in
+            NavigationLink {
+                ProfileDetailView(profile: profile)
+            } label: {
+                HStack {
+                    ProfileImageView(profile: profile, size: .init(width: 150, height: 150), drawRound: true)
+                    VStack(alignment: .leading) {
+                        Text(profile.name)
+                            .font(.system(size: 18))
+                            .bold()
+                        if let inbody = profile.inbodys.filter("deleted = %@", false).last {
+                            Divider()
+                            
                             HStack {
-                                ProfileImageView(profile: profile, size: .init(width: 150, height: 150), drawRound: true)
-                                VStack(alignment: .leading) {
-                                    Text(profile.name)
-                                        .font(.system(size: 18))
-                                        .bold()
-                                    if let inbody = profile.inbodys.filter("deleted = %@", false).last {
-                                        Divider()
-                                        
-                                        HStack {
-                                            Text("weight :")
-                                                .foregroundStyle(.secondary)
-                                                .font(.system(size: 12))
-                                            Text(String(format: "%0.0f", inbody.weight))
-                                                .font(.system(size: 18, weight: .heavy))
-                                            Text("kg")
-                                                .foregroundStyle(.secondary)
-                                                .font(.system(size: 12))
-                                        }
-                                        
-                                        HStack {
-                                            Text("inbody point :")
-                                                .foregroundStyle(.secondary)
-                                                .font(.system(size: 12))
-                                            Text(String(format: "%0.0f", inbody.inbodyPoint))
-                                                .font(.system(size: 18, weight: .heavy))
-                                        }
-                                        
-                                        HStack {
-                                            Text("BMI :")
-                                                .foregroundStyle(.secondary)
-                                                .font(.system(size: 12))
-                                            Text(String(format: "%0.1f", inbody.bmi))
-                                                .font(.system(size: 18, weight: .heavy))
-                                        }
-                                    }
-                                }
+                                Text("weight :")
+                                    .foregroundStyle(.secondary)
+                                    .font(.system(size: 12))
+                                Text(String(format: "%0.0f", inbody.weight))
+                                    .font(.system(size: 18, weight: .heavy))
+                                Text("kg")
+                                    .foregroundStyle(.secondary)
+                                    .font(.system(size: 12))
+                            }
+                            
+                            HStack {
+                                Text("inbody point :")
+                                    .foregroundStyle(.secondary)
+                                    .font(.system(size: 12))
+                                Text(String(format: "%0.0f", inbody.inbodyPoint))
+                                    .font(.system(size: 18, weight: .heavy))
+                            }
+                            
+                            HStack {
+                                Text("BMI :")
+                                    .foregroundStyle(.secondary)
+                                    .font(.system(size: 12))
+                                Text(String(format: "%0.1f", inbody.bmi))
+                                    .font(.system(size: 18, weight: .heavy))
                             }
                         }
                     }
                 }
+            }
+        }
+    }
+        
+    var body: some View {
+        List {
+            Section {
+                profiles.count == 0
+                ? Image("launchIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(20)
+                : profileList
             }
             Section("ad") {
                 NativeAdView()
